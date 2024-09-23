94e7c4248c056672ee847ac4065b4d9a7f72fb14
Leralubiteklery <lerasavchenk@gmail.com>
2024-09-23 15:34:21 +0300

    Add player plane object to GameScene

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -10,6 +10,8 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var player: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
         
         let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
@@ -31,5 +33,8 @@ class GameScene: SKScene {
             self.addChild(cloud)
             
         }
+        
+        player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
+        self.addChild(player)
     }
 }
