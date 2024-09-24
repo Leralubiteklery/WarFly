1d59829015108da096c38b88b69533d0eb4bb3da
Leralubiteklery <lerasavchenk@gmail.com>
2024-09-24 11:57:59 +0300

    Added logic for generation of clouds and islands.

--- WarFly/Cloud.swift
+++ WarFly/Cloud.swift
@@ -8,20 +8,6 @@
 import SpriteKit
 import GameplayKit
 
-protocol GameBackgroundSpritable {
-    static func populate() -> Self
-    static func randomPoint() -> CGPoint
-}
-
-extension GameBackgroundSpritable {
-    static func randomPoint() -> CGPoint {
-        let screen = UIScreen.main.bounds
-        let distribution = GKRandomDistribution(lowestValue: Int(screen.size.height) + 100, highestValue: Int(screen.size.height) + 200)
-        let y = CGFloat(distribution.nextInt())
-        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
-        return CGPoint(x: x, y: y)
-    }
-}
 
 final class Cloud: SKSpriteNode, GameBackgroundSpritable {
     
@@ -30,6 +16,17 @@ final class Cloud: SKSpriteNode, GameBackgroundSpritable {
         let cloud = Cloud(imageNamed: cloudImageName)
         cloud.setScale(randomScaleFactor)
         cloud.position = randomPoint()
+        cloud.zPosition = 10
+        cloud.run(move(from: cloud.position))
+        
+        return cloud
+    }
+    
+    static func populate(at point: CGPoint) -> Cloud {
+        let cloudImageName = configureName()
+        let cloud = Cloud(imageNamed: cloudImageName)
+        cloud.setScale(randomScaleFactor)
+        cloud.position = point
         cloud.zPosition = 10
         cloud.run(move(from: cloud.position))
         
--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -18,6 +18,32 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         configureStartScene()
+        spawnClouds()
+        spawnIslands()
+    }
+    
+    fileprivate func spawnClouds() {
+        let spawnCloudWait = SKAction.wait(forDuration: 1)
+        let spawnCloudAction = SKAction.run {
+            let cloud = Cloud.populate()
+            self.addChild(cloud)
+        }
+        
+        let spawnCloudSequence = SKAction.sequence([spawnCloudWait, spawnCloudAction])
+        let spawnCloudForever = SKAction.repeatForever(spawnCloudSequence)
+        run(spawnCloudForever)
+    }
+    
+    fileprivate func spawnIslands() {
+        let spawnIslandWait = SKAction.wait(forDuration: 2)
+        let spawnIslandAction = SKAction.run {
+            let island = Island.populate()
+            self.addChild(island)
+        }
+        
+        let spawnIslandSequence = SKAction.sequence([spawnIslandWait, spawnIslandAction])
+        let spawnIslandForever = SKAction.repeatForever(spawnIslandSequence)
+        run(spawnIslandForever)
     }
     
     fileprivate func configureStartScene() {
@@ -33,6 +59,12 @@ class GameScene: SKScene {
         
         let island2 = Island.populate(at: CGPoint(x: self.size.width - 100, y: self.size.height - 200))
         self.addChild(island2)
+        
+        let cloud1 = Cloud.populate(at: CGPoint(x: 100, y: 200))
+        self.addChild(cloud1)
+        
+        let cloud2 = Cloud.populate(at: CGPoint(x: self.size.width - 100, y: self.size.height - 200))
+        self.addChild((cloud2))
         
         player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
         self.addChild(player)
