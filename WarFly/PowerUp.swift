672c48f2cc4b1ec126cb124b6a04b241eec84cc2
Leralubiteklery <lerasavchenk@gmail.com>
2024-10-02 10:44:09 +0300

    PowerUp code refactoring.

--- WarFly/PowerUp.swift
+++ WarFly/PowerUp.swift
@@ -11,7 +11,7 @@ import SpriteKit
 class PowerUp: SKSpriteNode {
     let initialSize = CGSize(width: 52, height: 52)
     let textureAtlas = SKTextureAtlas(named: "GreenPowerUp")
-    var animationAray = [SKTexture]()
+    var animationSpriteArray = [SKTexture]()
     
     init() {
         let greenTexture = textureAtlas.textureNamed("missle_green_01")
@@ -26,15 +26,15 @@ class PowerUp: SKSpriteNode {
     
     func performRotation() {
         for i in 1...15 {
-            let number = String(format: "$02d", i)
-            animationAray.append(SKTexture(imageNamed: "missle_green_\(number)"))
+            let number = String(format: "%02d", i)
+            animationSpriteArray.append(SKTexture(imageNamed: "missle_green_\(number)"))
         }
         
-        SKTexture.preload(animationAray) {
-            let rotation = SKAction.animate(with: self.animationAray, timePerFrame: 0.05, resize: true, restore: false)
+        SKTexture.preload(animationSpriteArray) {
+            let rotation = SKAction.animate(with: self.animationSpriteArray, timePerFrame: 0.05, resize: true, restore: false)
             let rotationForever = SKAction.repeatForever(rotation)
             self.run(rotationForever)
         }
     }
-    
 }
+
