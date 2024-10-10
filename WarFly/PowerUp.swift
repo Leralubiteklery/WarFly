31ab6f99cfe618a4674f4c3ea020d608c41768d4
Leralubiteklery <lerasavchenk@gmail.com>
2024-10-10 12:56:39 +0300

    Created separate classes for Green and Blue Power ups.

--- WarFly/PowerUp.swift
+++ WarFly/PowerUp.swift
@@ -8,6 +8,28 @@
 import UIKit
 import SpriteKit
 
+class BluePowerUp: PowerUp {
+    init() {
+        let textureAtlas = SKTextureAtlas(named: "BluePowerUp")
+        super.init(textureAtlas: textureAtlas)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class GreenPowerUp: PowerUp {
+    init() {
+        let textureAtlas = SKTextureAtlas(named: "GreenPowerUp")
+        super.init(textureAtlas: textureAtlas)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class PowerUp: SKSpriteNode {
     let initialSize = CGSize(width: 52, height: 52)
     let textureAtlas: SKTextureAtlas!
